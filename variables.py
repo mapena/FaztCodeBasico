200c89d78c463c774f6fd64f11d506e1a641b0bd
mapena <mapena@gmail.com>
2020-09-05 12:33:46 -0300

    .

---
+++ variables.py
@@ -0,0 +1,21 @@
+name = "marcelo"
+print(name)
+name = None
+print(name)
+
+var1,var2 = "var1" , 5
+
+print(var1)
+print(var2)
+print(var1,var2)
+
+PI=3.14 # por convension las variables que no cambian (como si fuese constante) se
+        # las ecribe con mayuscula
+APP_NAME = "myprogram" # defino una varialbe que no cambiara de vlor (todo en mayuscula)
+print(PI)
+
+lista=[10, 20, 30]
+print(lista)
+
+tupla=(10, 20, 30)
+print(tupla)
