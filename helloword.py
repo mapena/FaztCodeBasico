a481479263f6b4f845cfa48db1213eca9116e468
mapena <mapena@gmail.com>
2020-09-05 12:05:08 -0300

    .

--- helloword.py
+++ helloword.py
@@ -2,4 +2,18 @@
 """ comentario
     comentario
 """
+# string todos diferentes
 print("helloword")
+print("""helloword""")
+print('helloword')
+print('''helloword''')
+print("hola" + "mundo") # concatena texto
+print(type('''helloword''')) # type dice que tipo de datos es
+
+#numeros int
+print(type(100)) # type dice que tipo de datos es
+#numeros float
+print(type(100.5)) # type dice que tipo de datos es
+
+#bool de booleano
+print(type(False))
