0e762892b2df7aef47a715c25122d7750350472d
mapena <mapena@gmail.com>
2020-09-07 12:46:18 -0300

    .

---
+++ modulos.py
@@ -0,0 +1,13 @@
+""" 
+los modulos pueden ser:
+1 nuestros
+2 terceras partes
+3 por python
+"""
+
+# 3 - por python  == https://docs.python.org/3/library/datetime.htm
+# se puede buscar : python modules
+import datetime
+
+print(datetime.date.today()) # nos da le fecha
+print(datetime.timedelta(minutes=70)) # convierte 70 minutos en horas
---
+++ modulos2.py
@@ -0,0 +1,11 @@
+ from datetime import timedelta # estoy import timedelta de datetime 
+ from datetime import date # estoy import date de datetime 
+ import fmath # importo mis librerias
+
+ print(timedelta(minutes=70)) # convierte 70
+ print(date.today()) # nos da le fecha
+
+ fmath.sumar(2,3) # invoco la libreria sumar
+#---------------------------------------------------
+#from fmath import sumar
+#pipsumar(4,5)
