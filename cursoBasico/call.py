2bf7720560dc6f94de6aa94dffd5f554921496a9
mapena <38469179+mapena@users.noreply.github.com>
2020-09-15 15:07:33 -0300

    Update call.py

--- cursoBasico/call.py
+++ cursoBasico/call.py
@@ -1,4 +1,6 @@
 import subprocess
 #subprocess.run(["mkdir", "myfolder"])
-subprocess.run(["ls", "-l"])
-#subprocess.run("dir")
+#subprocess.run(["ls", "-l"])
+#subprocess.run(["mkdir", "-l"],shell=TRUE)  # el comando lo ejecuta sobre el shell
+subprocess.run(["mkdir", "-l"],shell=FALSE)  # es lo recomendable para que primero el comando se ejecute en el interprete de python
+
